b0427f5dc66711a5a40db3a83d1f21dbc299d5f3
CsCodeway <cscodeway@gmail.com>
2023-07-08 17:09:23 +0530

    zoom test

--- app/page.js
+++ app/page.js
@@ -1,6 +1,17 @@
 import WeatherCard from "@/components/WeatherCard";
+import Head from "next/head";
 
 const App = () => {
-  return <WeatherCard />;
+  return (
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, user-scalable=1, maximum-scale=1"
+        />
+      </Head>
+      <WeatherCard />
+    </>
+  );
 };
 export default App;
